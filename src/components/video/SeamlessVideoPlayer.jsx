74630dd69bc36404160651ba5c87ce756c883204
RUBIS <id@rubis.snu.ac.kr>
2024-11-19 00:16:23 +0900

    상태변수관리 2

--- src/components/video/SeamlessVideoPlayer.jsx
+++ src/components/video/SeamlessVideoPlayer.jsx
@@ -117,7 +117,7 @@ const SeamlessVideoPlayer = forwardRef((props, ref) => {
         }
         await sleep(RETRY_DELAY);
         // Continue loop to retry
-        if (retryCounts.current[index] >= 10 && onError) {
+        if (retryCounts.current[index] >= 61 && onError) {
           onError(error);
           return;
         }
